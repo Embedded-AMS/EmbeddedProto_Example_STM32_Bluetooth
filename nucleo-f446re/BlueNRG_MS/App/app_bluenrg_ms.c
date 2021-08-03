cbc100c431fe8f7c57fb4da3545595d539fe8298
Bart Hertog <Bart@EmbeddedAMS.nl>
2021-08-03 16:47:41 +0200

    Removed commented out code.

--- nucleo-f446re/BlueNRG_MS/App/app_bluenrg_ms.c
+++ nucleo-f446re/BlueNRG_MS/App/app_bluenrg_ms.c
@@ -242,34 +242,6 @@ static void User_Process(void)
     setConnectable();
     set_connectable = FALSE;
   }  
-
-
-  /* Check if the User Button has been pushed */    
-//  if (user_button_pressed)
-//  {
-//    /* Debouncing */
-//    HAL_Delay(50);
-//
-//    /* Wait until the User Button is released */
-//    while (BSP_PB_GetState(BUTTON_KEY) == !user_button_init_state);
-//
-//    /* Debouncing */
-//    HAL_Delay(50);
-//
-//    BSP_LED_Toggle(LED2);
-//
-//    if (connected)
-//    {
-//      /* Update acceleration data */
-//      p_axes->AXIS_X += 100;
-//      p_axes->AXIS_Y += 100;
-//      p_axes->AXIS_Z += 100;
-//     Acc_Update(p_axes);
-//    }
-//
-//    /* Reset the User Button flag */
-//    user_button_pressed = 0;
-//  }
 }
 
 /**
